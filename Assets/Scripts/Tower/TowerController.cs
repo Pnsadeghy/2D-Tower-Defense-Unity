e472f5141cf6dde93b00ebe2f97049a73155bf96
Pouria <pnsadeghy@gmail.com>
2023-03-07 15:21:13 +0330

    Tower controller/data

--- Assets/Scripts/Tower/TowerController.cs
+++ Assets/Scripts/Tower/TowerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Helper;
 using Tower.Data;
 using UnityEngine;
 
@@ -15,6 +16,88 @@ namespace Tower
         
         [SerializeField]
         private Transform container;
+
+        [SerializeField]
+        private LayerMask enemyMask;
+
+        #endregion
+
+        #region Variables
+
+        private Transform _enemy;
+
+        private float _lastShot;
+
+        #endregion
+
+        #region Events
+
+        private void Update()
+        {
+            CheckEnemy();
+            CheckEnemies();
+            
+            LookAt();
+            CheckShoot();
+        }
+
+        #endregion
+
+        #region Set
+
+        private void LookAt()
+        {
+            if (!HasEnemy()) return;
+            container.rotation = ValueHelper.LookAt2D(container.position, _enemy.position);
+        }
+
+        private void Shoot()
+        {
+            _lastShot = Time.time;
+            // #TODO animation
+            // #TODO create bullet
+        }
+
+        #endregion
+
+        #region Check
+
+        private bool HasEnemy() => _enemy != null;
+        
+        private void CheckShoot()
+        {
+            if (!HasEnemy()) return;
+            if (Time.time < _lastShot + towerData.timeout) return;
+            Shoot();
+        }
+        
+        private void CheckEnemy()
+        {
+            if (HasEnemy() && Vector3.Distance(transform.position, _enemy.position) > towerData.range)
+            {
+                _enemy = null;
+            }
+        }
+        
+        private void CheckEnemies()
+        {
+            if (HasEnemy()) return;
+
+            var currentPosition = transform.position;
+            var colliders = Physics2D.OverlapCircleAll(currentPosition, towerData.range, enemyMask);
+
+            Transform nearestEnemy = null;
+            var nearestDistance = Mathf.Infinity;
+            foreach (var enemy in colliders)
+            {
+                var distance = Vector3.Distance(currentPosition, enemy.transform.position);
+                if (!(distance < nearestDistance)) continue;
+                nearestDistance = distance;
+                nearestEnemy = enemy.transform;
+            }
+
+            _enemy = nearestEnemy;
+        }
 
         #endregion
 
