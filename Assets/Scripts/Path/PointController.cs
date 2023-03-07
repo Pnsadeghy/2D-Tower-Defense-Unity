495c7356be793ca3648c01fbdafe8f3c6b13f2b3
Pouria <pnsadeghy@gmail.com>
2023-03-07 13:21:09 +0330

    Point Controller Set Next points

---
+++ Assets/Scripts/Path/PointController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Path
+{
+    public class PointController : MonoBehaviour
+    {
+        [SerializeField]
+        private List<PointController> nextPoints;
+    }
+}
