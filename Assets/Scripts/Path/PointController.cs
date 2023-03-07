f9cc099f5b81b86caf10ee9e15af68f423fec44c
Pouria <pnsadeghy@gmail.com>
2023-03-07 13:24:52 +0330

    Next point helper

--- Assets/Scripts/Path/PointController.cs
+++ Assets/Scripts/Path/PointController.cs
@@ -7,7 +7,19 @@ namespace Path
 {
     public class PointController : MonoBehaviour
     {
+        #region Properties
+
         [SerializeField]
         private List<PointController> nextPoints;
+
+        #endregion
+
+        #region Helper
+
+        // Get next point by random
+        public PointController GetNextPoint() =>
+            nextPoints.Count.Equals(0) ? null : nextPoints[Random.Range(0, nextPoints.Count)];
+
+        #endregion
     }
 }
