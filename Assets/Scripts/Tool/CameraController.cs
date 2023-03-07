ca958a386273d4ef33c2218541625f10ac4f21b7
Pouria <pnsadeghy@gmail.com>
2023-03-07 11:34:11 +0330

    Camera Script/Config file

---
+++ Assets/Scripts/Tool/CameraController.cs
@@ -0,0 +1,11 @@
+using Tool.Data;
+using UnityEngine;
+
+namespace Tool
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] 
+        private CameraLevelData cameraData;
+    }
+}
