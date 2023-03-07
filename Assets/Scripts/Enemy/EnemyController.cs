83b5fbaeef64dafab77151faaccd9504c579f257
Pouria <pnsadeghy@gmail.com>
2023-03-07 14:27:42 +0330

    Enemy healthbar

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -18,6 +18,9 @@ namespace Enemy
         
         [SerializeField]
         private Transform sprite;
+        
+        [SerializeField]
+        private Transform healthBar;
 
         #endregion
 
